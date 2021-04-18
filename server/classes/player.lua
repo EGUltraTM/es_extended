2f965e4547b5e6ee7d323efbc0e6c18b285ff37e
Linden <65407488+thelindat@users.noreply.github.com>
2021-04-19 06:15:10 +1000

    Fixed missing code

--- server/classes/player.lua
+++ server/classes/player.lua
@@ -195,12 +195,41 @@ function CreateExtendedPlayer(playerId, identifier, group, accounts, inventory, 
 		return exports['linden_inventory']:setMaxWeight(self, newWeight)
 	end
 
+	self.setJob = function(job, grade)
+		grade = tostring(grade)
+		local lastJob = json.decode(json.encode(self.job))
+
+		if ESX.DoesJobExist(job, grade) then
+			local jobObject, gradeObject = ESX.Jobs[job], ESX.Jobs[job].grades[grade]
+
+			self.job.id    = jobObject.id
+			self.job.name  = jobObject.name
+			self.job.label = jobObject.label
+
+			self.job.grade        = tonumber(grade)
+			self.job.grade_name   = gradeObject.name
+			self.job.grade_label  = gradeObject.label
+			self.job.grade_salary = gradeObject.salary
+
+			if gradeObject.skin_male then
+				self.job.skin_male = json.decode(gradeObject.skin_male)
+			else
+				self.job.skin_male = {}
+			end
+
+			if gradeObject.skin_female then
+				self.job.skin_female = json.decode(gradeObject.skin_female)
+			else
+				self.job.skin_female = {}
+			end
+
 			TriggerEvent('esx:setJob', self.source, self.job, lastJob)
 			self.triggerEvent('esx:setJob', self.job)
 		else
 			print(('[es_extended] [^3WARNING^7] Ignoring invalid .setJob() usage for "%s"'):format(self.identifier))
 		end
 	end
+
 
 	self.showNotification = function(msg)
 		self.triggerEvent('esx:showNotification', msg)
