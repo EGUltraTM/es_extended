ff69fef720b94173aa797d0f857a5d5fd20a1ea9
EGUltraTM <31127586+EGUltraTM@users.noreply.github.com>
2021-04-25 13:13:32 +0200

    Fixed

--- client/main.lua
+++ client/main.lua
@@ -1,5 +1,16 @@
 local isPaused, isDead, pickups = false, false, {}
 
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+
+		if NetworkIsPlayerActive(PlayerId()) then
+			TriggerServerEvent('esx:onPlayerJoined')
+			break
+		end
+	end
+end)
+
 RegisterNetEvent('esx:playerLoaded')
 AddEventHandler('esx:playerLoaded', function(playerData)
 	ESX.PlayerLoaded = true
